ebd11bb3588ed129eb5849281aaf6e0d9e4f54d7
Richie Ward <RichieS@GMail.com>
2020-07-28 21:07:47 +0100

    Use a script to get latest version.

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
 docker login docker.pkg.github.com/rich43
-docker-compose up -d
+CONTENT_SERVER_RELEASE=`get_latest_release "Rich43/archesky-content-server"` AUTH_SERVER_RELEASE=`get_latest_release "Rich43/archesky-auth-server"` docker-compose up -d
