1663d0260406a4423798cc7a9fa21de8f8d6c54d
Richie Ward <RichieS@GMail.com>
2020-08-16 16:08:18 +0100

    Send realm to server, wip

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Docker files for archesky
 Getting Started
 ---------------
 * `./run.sh`
-* `apt install python3-pycurl`
+* `apt install python3-requests`
 * `cd keycloak`
 * (You may want to modify this command)
     * `./add_realm.py --url http://localhost:8180 --username root --password Password1 --domain localhost --port 3000`
--- keycloak/add_realm.py
+++ keycloak/add_realm.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 
+import requests
+
 parser = argparse.ArgumentParser(description='Add a realm to keycloak.')
 parser.add_argument('--url', required=True, type=str,
                     help='Keycloak url e.g. http://localhost:8080')
@@ -13,4 +15,32 @@ parser.add_argument('--domain', required=True, type=str,
 parser.add_argument('--port', required=False, type=int,
                     help='Realm port e.g. 3000 - this is optional')
 args = parser.parse_args()
-print(args)
+
+if args.port:
+    args.port = f":{args.port}"
+else:
+    args.port = ""
+
+response = requests.post(
+    f"{args.url}/auth/realms/master/protocol/openid-connect/token",
+    {
+        "client_id": "admin-cli",
+        "username": args.username,
+        "password": args.password,
+        "grant_type": "password"
+    }
+)
+
+token = response.json()['access_token']
+headers = {'Authorization': f"Bearer {token}"}
+data: str = open("archesky.json").read()
+data = data.replace("$domainname", args.domain)
+data = data.replace("$port", args.port)
+
+response = requests.post(
+    f"{args.url}/auth",
+    data,
+    headers=headers
+)
+
+print(response.text)
